e05dbb955322a9a007d3903dccf0bde2c4d4198d
Wonseoop <123632194+Wonseoop@users.noreply.github.com>
2023-05-20 12:53:20 +0900

    배열 동적 할당

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -34,12 +34,20 @@ MainWindow::MainWindow(QWidget *parent)
 
     file.close();
 
-    QLabel* cells[51][46];
-    int row=51;
-    int col=46;
 
-    // 미로 셀 생성 및 UI에 추가
+    int row = maze.size();  // 행의 크기
+    int col = 0;            // 열의 크기
+
+    if (row > 0) {
+            col = maze[0].size();  // 첫 번째 행의 열의 크기를 기준으로 설정
+    }
+
+
+
+
+    QLabel*** cells = new QLabel**[row];                                                            // 배열 동적 할당
     for (int i = 0; i < row; ++i) {
+            cells[i] = new QLabel*[col];
             for (int j = 0; j < col; ++j) {
                 QLabel* cell = new QLabel(ui->tableWidget);
                 cell->setAlignment(Qt::AlignCenter);
@@ -47,7 +55,6 @@ MainWindow::MainWindow(QWidget *parent)
 
                 if (maze[i][j] == "0") {
                     cell->setText("0");  // 통로
-
                     cell->setStyleSheet("QLabel { background-color : white; color : white; }");
                 } else {
                     cell->setText("1");  // 벽
@@ -59,6 +66,16 @@ MainWindow::MainWindow(QWidget *parent)
             }
     }
 
+    // 배열 해제 코드
+    /*for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                delete cells[i][j];
+            }
+            delete[] cells[i];
+    }
+    delete[] cells;
+    */
+
 
 
 
@@ -66,5 +83,6 @@ MainWindow::MainWindow(QWidget *parent)
 
 MainWindow::~MainWindow()
 {
+
     delete ui;
 }
