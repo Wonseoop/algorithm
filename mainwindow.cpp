120604e73591099f8c506ab8f452fe977882f785
Wonseoop <123632194+Wonseoop@users.noreply.github.com>
2023-05-20 15:12:28 +0900

    미로 경로 입력받기

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,38 +15,56 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    QFile file("C:\\Users\\원현섭\\Documents\\untitled1\\maze1_kt.txt");
-    if(!file.open(QIODevice::ReadOnly)) {
-        ui->label->setText("없어");
-    }
-    else{
-        ui->label->setText("있어");
-    }
 
-    QTextStream in(&file);
-    QList<QStringList> maze;
 
-    while(!in.atEnd()) {
+}
+
+MainWindow::~MainWindow()
+{
+
+    delete ui;
+}
+
+void MainWindow::on_lineEdit_returnPressed()        //파일 경로 입력하면 미로 출력해주는 메서드
+{
+
+
+
+        QString fileName = ui->lineEdit->text();
+        QFile file(fileName);
+        //QFile file("C:\\Users\\원현섭\\Documents\\untitled1\\maze2_kt.txt");
+
+        if(!file.open(QIODevice::ReadOnly)) {
+            ui->label->setText("파일이 존재하지 않습니다. 경로 재확인 요망");
+        }
+        else{
+            ui->label->setText("파일이 존재합니다.");
+        }
+
+        QTextStream in(&file);
+        QList<QStringList> maze;
+
+        while(!in.atEnd()) {
             QString line = in.readLine();
             QStringList list1 = line.split(' ');
             maze.append(list1);      //2차원 배열 생성
-    }
+        }
 
-    file.close();
+        file.close();
 
 
-    int row = maze.size();  // 행의 크기
-    int col = 0;            // 열의 크기
+        int row = maze.size();  // 행의 크기
+        int col = 0;            // 열의 크기
 
-    if (row > 0) {
+        if (row > 0) {
             col = maze[0].size();  // 첫 번째 행의 열의 크기를 기준으로 설정
-    }
+        }
 
 
 
 
-    QLabel*** cells = new QLabel**[row];                                                            // 배열 동적 할당
-    for (int i = 0; i < row; ++i) {
+        QLabel*** cells = new QLabel**[row];                                                            // 배열 동적 할당
+        for (int i = 0; i < row; ++i) {
             cells[i] = new QLabel*[col];
             for (int j = 0; j < col; ++j) {
                 QLabel* cell = new QLabel(ui->tableWidget);
@@ -64,25 +82,9 @@ MainWindow::MainWindow(QWidget *parent)
                 cells[i][j] = cell;
                 ui->gridLayout->addWidget(cell, i, j);
             }
-    }
-
-    // 배열 해제 코드
-    /*for (int i = 0; i < row; ++i) {
-            for (int j = 0; j < col; ++j) {
-                delete cells[i][j];
-            }
-            delete[] cells[i];
-    }
-    delete[] cells;
-    */
-
+        }
 
 
 
 }
 
-MainWindow::~MainWindow()
-{
-
-    delete ui;
-}
